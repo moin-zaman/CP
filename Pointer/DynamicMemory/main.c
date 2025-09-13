fa04a322079c90d4da2e35fbe8f353f0872fb0f3
moin-zaman <moinkhan758abzx@gmail.com>
2025-09-13 17:02:03 +0600

    Added DM first file

---
+++ Pointer/DynamicMemory/main.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//void* malloc(int size)
+//{
+//
+//}
+
+int main()
+{
+//    int *px = malloc(4);
+
+
+//    px = (int*)malloc(4);
+
+    int *px = (int*)malloc(4);
+
+    char *pxx = (char*)malloc(1);
+
+}
